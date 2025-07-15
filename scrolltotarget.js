daa4c08ec7b99b22e8d2c6e9fed10545dba0f782
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-07-15 11:12:56 +0200

    Update scrolltotarget.js

--- scrolltotarget.js
+++ scrolltotarget.js
@@ -1,10 +1,8 @@
-        // Function to scroll to the corresponding heading div block based on the clicked image
-        function scrollToTarget(targetName) {
-            // Get the target block by its ID using the passed text name
-            const target = document.getElementById('section-' + targetName);
-
-            // Scroll to the target element with smooth behavior
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+window.scrollToTarget = function(targetName) {
+    const target = document.getElementById('section-' + targetName);
+    if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.error(`Element with ID section-${targetName} not found`);
+    }
+};
