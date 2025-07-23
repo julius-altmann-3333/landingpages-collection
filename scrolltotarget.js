f75ba3bd10408a3a28777761050daad3dbae204e
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-07-23 14:43:02 +0200

    Update scrolltotarget.js

--- scrolltotarget.js
+++ scrolltotarget.js
@@ -1,8 +1,7 @@
-window.scrollToTarget = function(targetName) {
-    const target = document.getElementById('section-' + targetName);
-    if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
-    } else {
-        console.error(`Element with ID section-${targetName} not found`);
-    }
-};
+document.addEventListener('DOMContentLoaded', () => {
+    window.scrollToTarget = function(targetName) {
+        const target = document.getElementById('section-' + targetName);
+        if (target) target.scrollIntoView({ behavior: 'smooth' });
+        else console.error(`Element with ID section-${targetName} not found`);
+    };
+});
